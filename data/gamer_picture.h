3df790b5f64a4b70d6dd597dd3d6ceda365282ab
tobii-dev <77458451+tobii-dev@users.noreply.github.com>
2025-02-15 00:02:42 +0100

    mv: data/

---
+++ data/gamer_picture.h
@@ -0,0 +1,315 @@
+typedef unsigned char   undefined;
+
+typedef unsigned char    bool;
+typedef unsigned char    byte;
+typedef unsigned int    dword;
+float4
+float8
+typedef long double    longdouble;
+typedef long long    longlong;
+typedef unsigned long long    qword;
+typedef int    sdword;
+typedef long long    sqword;
+typedef short    sword;
+typedef unsigned char    uchar;
+typedef unsigned int    uint;
+typedef unsigned long    ulong;
+typedef unsigned long long    ulonglong;
+typedef unsigned char    undefined1;
+typedef unsigned short    undefined2;
+typedef unsigned int    undefined3;
+typedef unsigned int    undefined4;
+typedef unsigned long long    undefined5;
+typedef unsigned long long    undefined6;
+typedef unsigned long long    undefined7;
+typedef unsigned long long    undefined8;
+typedef unsigned short    ushort;
+typedef short    wchar_t;
+typedef unsigned short    word;
+typedef struct I_GamerPictureManager I_GamerPictureManager, *PI_GamerPictureManager;
+
+typedef struct Thread Thread, *PThread;
+
+typedef struct Vector<UI::C_GamerPicture*,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_> Vector<UI::C_GamerPicture*,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_>, *PVector<UI::C_GamerPicture*,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_>;
+
+typedef struct SceNpCommunicationId SceNpCommunicationId, *PSceNpCommunicationId;
+
+typedef struct SceNpCommunicationId TTitleID;
+
+typedef struct Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_> Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_>, *PVector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_>;
+
+typedef struct Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Static<10>_> Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Static<10>_>, *PVector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Static<10>_>;
+
+typedef struct C_EventBase C_EventBase, *PC_EventBase;
+
+typedef qword uint64_t;
+
+typedef uint64_t sys_ppu_thread_t;
+
+typedef sys_ppu_thread_t ThreadHandle;
+
+typedef sdword S32;
+
+typedef struct VectorStructure_Dynamic<UI::C_GamerPicture*,Btl::ArrayAllocator<UI::C_GamerPicture*>_> VectorStructure_Dynamic<UI::C_GamerPicture*,Btl::ArrayAllocator<UI::C_GamerPicture*>_>, *PVectorStructure_Dynamic<UI::C_GamerPicture*,Btl::ArrayAllocator<UI::C_GamerPicture*>_>;
+
+typedef uchar uint8_t;
+
+typedef struct VectorStructure_Dynamic<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<UI::I_GamerPictureManager::RemoteRequest>_> VectorStructure_Dynamic<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<UI::I_GamerPictureManager::RemoteRequest>_>, *PVectorStructure_Dynamic<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<UI::I_GamerPictureManager::RemoteRequest>_>;
+
+typedef struct VectorStructure_Static<UI::I_GamerPictureManager::RemoteRequest,10> VectorStructure_Static<UI::I_GamerPictureManager::RemoteRequest,10>, *PVectorStructure_Static<UI::I_GamerPictureManager::RemoteRequest,10>;
+
+typedef struct C_WaitObject C_WaitObject, *PC_WaitObject;
+
+typedef struct DynamicVectorBase<Btl::DynamicArrayData<UI::C_GamerPicture*,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_>_> DynamicVectorBase<Btl::DynamicArrayData<UI::C_GamerPicture*,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_>_>, *PDynamicVectorBase<Btl::DynamicArrayData<UI::C_GamerPicture*,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_>_>;
+
+typedef struct DynamicVectorBase<Btl::DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_>_> DynamicVectorBase<Btl::DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_>_>, *PDynamicVectorBase<Btl::DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_>_>;
+
+typedef struct VectorStructure_Array<UI::I_GamerPictureManager::RemoteRequest,10> VectorStructure_Array<UI::I_GamerPictureManager::RemoteRequest,10>, *PVectorStructure_Array<UI::I_GamerPictureManager::RemoteRequest,10>;
+
+typedef dword U32;
+
+typedef dword uint32_t;
+
+typedef uint32_t sys_event_queue_t;
+
+typedef uint32_t sys_event_port_t;
+
+typedef struct sys_event sys_event, *Psys_event;
+
+typedef struct sys_event sys_event_t;
+
+typedef void * HANDLE;
+
+typedef uint DWORD;
+
+typedef struct DynamicArrayData<UI::C_GamerPicture*,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_> DynamicArrayData<UI::C_GamerPicture*,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_>, *PDynamicArrayData<UI::C_GamerPicture*,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_>;
+
+typedef struct DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_> DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_>, *PDynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_>;
+
+typedef struct SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,10> SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,10>, *PSameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,10>;
+
+typedef struct C_GamerPicture C_GamerPicture, *PC_GamerPicture;
+
+typedef struct RemoteRequest RemoteRequest, *PRemoteRequest;
+
+typedef struct AlignedObject<4> AlignedObject<4>, *PAlignedObject<4>;
+
+typedef struct I_AlternateTexture I_AlternateTexture, *PI_AlternateTexture;
+
+typedef uchar U8;
+
+typedef struct UserIdentifier UserIdentifier, *PUserIdentifier;
+
+typedef struct C_FixedString<32> C_FixedString<32>, *PC_FixedString<32>;
+
+typedef struct C_FixedString<32> C_String32;
+
+typedef struct C_Texture C_Texture, *PC_Texture;
+
+typedef struct C_Texture C_Texture2D;
+
+typedef C_Texture * UITextureHandle;
+
+typedef struct SceNpId SceNpId, *PSceNpId;
+
+typedef char CHAR8;
+
+typedef struct CellGcmTexture CellGcmTexture, *PCellGcmTexture;
+
+typedef struct CellGcmTexture GPUTexture;
+
+typedef struct SceNpOnlineId SceNpOnlineId, *PSceNpOnlineId;
+
+typedef word uint16_t;
+
+struct DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_> { /* Original name: DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest, 1> > > */
+    struct RemoteRequest * m_elements;
+    int m_capacity;
+};
+
+struct DynamicVectorBase<Btl::DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_>_> { /* Original name: DynamicVectorBase<Btl::DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest, Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest, 1> > > > */
+    struct DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_> elements;
+    int m_nElements;
+};
+
+struct DynamicArrayData<UI::C_GamerPicture*,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_> { /* Original name: DynamicArrayData<UI::C_GamerPicture*,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*, 1> > > */
+    struct C_GamerPicture * * m_elements;
+    int m_capacity;
+};
+
+struct DynamicVectorBase<Btl::DynamicArrayData<UI::C_GamerPicture*,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_>_> { /* Original name: DynamicVectorBase<Btl::DynamicArrayData<UI::C_GamerPicture*, Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*, 1> > > > */
+    struct DynamicArrayData<UI::C_GamerPicture*,Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_> elements;
+    int m_nElements;
+};
+
+struct VectorStructure_Dynamic<UI::C_GamerPicture*,Btl::ArrayAllocator<UI::C_GamerPicture*>_> { /* Original name: VectorStructure_Dynamic<UI::C_GamerPicture*,Btl::ArrayAllocator<UI::C_GamerPicture*> > */
+    struct DynamicVectorBase<Btl::DynamicArrayData<UI::C_GamerPicture*,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_>_> super_DynamicVectorBase<Btl::DynamicArrayData<UI::C_GamerPicture*,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::C_GamerPicture*,_1>_>_>_>;
+};
+
+struct C_EventBase {
+    uint64_t mId;
+    struct C_WaitObject * mWaitObject;
+    bool mFlag;
+};
+
+struct CellGcmTexture {
+    uint8_t format;
+    uint8_t mipmap;
+    uint8_t dimension;
+    uint8_t cubemap;
+    uint32_t remap;
+    uint16_t width;
+    uint16_t height;
+    uint16_t depth;
+    uint8_t location;
+    uint8_t _padding;
+    uint32_t pitch;
+    uint32_t offset;
+};
+
+struct C_Texture {
+    U32 m_Size;
+    U32 m_Flags;
+    GPUTexture m_gcmTexture;
+};
+
+struct SceNpOnlineId {
+    char data[16];
+    char term;
+    char dummy[3];
+};
+
+struct SceNpId {
+    struct SceNpOnlineId handle;
+    uint8_t opt[8];
+    uint8_t reserved[8];
+};
+
+struct UserIdentifier {
+    struct SceNpId m_Uid;
+    bool m_IsGuest;
+};
+
+struct C_FixedString<32> {
+    CHAR8 m_Chars[32];
+};
+
+struct I_AlternateTexture {
+    int (** _vptr.I_AlternateTexture)(...);
+};
+
+struct C_GamerPicture {
+    struct I_AlternateTexture super_I_AlternateTexture;
+    U8 m_Ref;
+    struct UserIdentifier m_UserData;
+    bool m_Active;
+    bool m_Free;
+    C_String32 m_Name;
+    undefined field6_0x4c;
+    undefined field7_0x4d;
+    undefined field8_0x4e;
+    undefined field9_0x4f;
+    C_Texture2D m_Texture;
+    UITextureHandle m_DefaultTexture;
+    undefined field12_0x74;
+    undefined field13_0x75;
+    undefined field14_0x76;
+    undefined field15_0x77;
+    undefined field16_0x78;
+    undefined field17_0x79;
+    undefined field18_0x7a;
+    undefined field19_0x7b;
+    undefined field20_0x7c;
+    undefined field21_0x7d;
+    undefined field22_0x7e;
+    undefined field23_0x7f;
+};
+
+struct sys_event {
+    uint64_t source;
+    uint64_t data1;
+    uint64_t data2;
+    uint64_t data3;
+};
+
+struct AlignedObject<4> {
+    U32 x;
+};
+
+struct SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,10> {
+    struct AlignedObject<4> data[110];
+};
+
+struct VectorStructure_Array<UI::I_GamerPictureManager::RemoteRequest,10> {
+    struct SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,10> storage;
+};
+
+struct VectorStructure_Static<UI::I_GamerPictureManager::RemoteRequest,10> {
+    struct VectorStructure_Array<UI::I_GamerPictureManager::RemoteRequest,10> super_VectorStructure_Array<UI::I_GamerPictureManager::RemoteRequest,10>;
+    U32 m_nElements;
+};
+
+struct Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Static<10>_> { /* Original name: Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Static<10> > */
+    struct VectorStructure_Static<UI::I_GamerPictureManager::RemoteRequest,10> super_VectorStructure_Static<UI::I_GamerPictureManager::RemoteRequest,10>;
+};
+
+struct SceNpCommunicationId {
+    char data[9];
+    char term;
+    uint8_t num;
+    char dummy;
+};
+
+struct Thread {
+    int (** _vptr.Thread)(...);
+    struct C_EventBase super_C_EventBase;
+    ThreadHandle * m_Handle;
+    S32 m_Priority;
+    bool m_bIsRunning;
+};
+
+struct Vector<UI::C_GamerPicture*,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_> { /* Original name: Vector<UI::C_GamerPicture*,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType> > > */
+    struct VectorStructure_Dynamic<UI::C_GamerPicture*,Btl::ArrayAllocator<UI::C_GamerPicture*>_> super_VectorStructure_Dynamic<UI::C_GamerPicture*,Btl::ArrayAllocator<UI::C_GamerPicture*>_>;
+};
+
+struct VectorStructure_Dynamic<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<UI::I_GamerPictureManager::RemoteRequest>_> { /* Original name: VectorStructure_Dynamic<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<UI::I_GamerPictureManager::RemoteRequest> > */
+    struct DynamicVectorBase<Btl::DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_>_> super_DynamicVectorBase<Btl::DynamicArrayData<UI::I_GamerPictureManager::RemoteRequest,_Btl::ArrayAllocator<Btl::SameSizeAndAlignment<UI::I_GamerPictureManager::RemoteRequest,_1>_>_>_>;
+};
+
+struct Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_> { /* Original name: Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType> > > */
+    struct VectorStructure_Dynamic<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<UI::I_GamerPictureManager::RemoteRequest>_> super_VectorStructure_Dynamic<UI::I_GamerPictureManager::RemoteRequest,Btl::ArrayAllocator<UI::I_GamerPictureManager::RemoteRequest>_>;
+};
+
+struct I_GamerPictureManager {
+    struct Thread super_Thread;
+    struct Vector<UI::C_GamerPicture*,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_> m_LocalGamerPictures;
+    struct Vector<UI::C_GamerPicture*,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_> m_RemoteGamerPictures;
+    bool m_Busy;
+    TTitleID m_TitleID;
+    undefined field5_0x49;
+    undefined field6_0x4a;
+    undefined field7_0x4b;
+    struct Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Dynamic<Btl::ArrayAllocator<Btl::NullType>_>_> m_RemoteRequestList;
+    struct Vector<UI::I_GamerPictureManager::RemoteRequest,Btl::VectorParameters_Static<10>_> m_RemoteJobList;
+    undefined field10_0x214;
+    undefined field11_0x215;
+    undefined field12_0x216;
+    undefined field13_0x217;
+};
+
+struct C_WaitObject {
+    sys_event_queue_t mEventQueue;
+    sys_event_port_t mEventPort;
+    sys_event_t mSysEvent;
+    HANDLE * mHandles;
+    DWORD mHandleCount;
+    bool mWaitForAll;
+    DWORD mMilliseconds;
+};
+
+struct RemoteRequest {
+    S32 slot;
+    struct UserIdentifier user;
+    bool local;
+};
+
